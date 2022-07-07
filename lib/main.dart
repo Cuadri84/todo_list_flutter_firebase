a734183bade3805dbd8c521afff427256ff1b00b
Cuadri84 <davidcuadrillerosaura@gmail.com>
2022-07-07 19:23:20 +0200

    first part of the connection to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/provider/todos.dart';
 import 'package:todo_list/screens/home.dart';
 
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   static const String title = 'TODO LIST';
